7024a798d9f49179795ff6c5c9c0e357d2813adb
Jake <9jparrish@gmail.com>
2018-06-11 18:51:06 -0400

    first

---
+++ Checkers/src/checkers/Checker.java
@@ -0,0 +1,25 @@
+package checkers;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Checker extends JFrame implements ActionListener {
+private JPanel a = new JPanel();
+
+public Checker() {
+	a.setLayout(new GridLayout());
+}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
