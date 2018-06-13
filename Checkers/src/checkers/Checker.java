5f34e40755cd502e796b780e287cd95578583ac1
Jake <9jparrish@gmail.com>
2018-06-12 21:45:25 -0400

    actually looks like a checkerboard

--- Checkers/src/checkers/Checker.java
+++ Checkers/src/checkers/Checker.java
@@ -1,5 +1,6 @@
 package checkers;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +14,9 @@ private JPanel checkerBoard = new JPanel();
 private JPanel play = new JPanel();
 private JButton letsPlay = new JButton("Lets play");
 private JButton[][] spots = new JButton[8][8];
+private boolean black = false;
+private boolean black2 = true;
+private boolean row = true;
 
 public Checker() {
 	super("Checker");
@@ -20,13 +24,41 @@ public Checker() {
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
 	play.setLayout(new GridLayout(2,3));
+	letsPlay.setBackground(Color.BLUE);
+	letsPlay.setForeground(Color.BLUE);
+	letsPlay.setOpaque(true);
 	letsPlay.addActionListener(this);
 	letsPlay.setActionCommand("letsplay");
+	play.setAlignmentX(CENTER_ALIGNMENT);
+	play.setAlignmentY(CENTER_ALIGNMENT);
 	play.add(letsPlay);
 	
 	for(int i = 0; i < spots.length; i++ ) {
+		if(i % 2 == 0) {
+			row = true;
+		} else {row = false;}
 		for(int j = 0; j < spots[i].length; j++) {
-			spots[i][j] = new JButton("hi");
+			spots[i][j] = new JButton();
+			
+			if(row) {
+			if(black) {
+				spots[i][j].setBackground(Color.BLACK);
+				black = false;
+			} else {
+				spots[i][j].setBackground(Color.WHITE);
+				black = true;
+			}
+			} else {
+				if(black2) {
+					spots[i][j].setBackground(Color.BLACK);
+					black2 = false;
+				} else {
+					spots[i][j].setBackground(Color.WHITE);
+					black2 = true;
+				}
+			}
+		
+			spots[i][j].setBorderPainted(false);
 			spots[i][j].setOpaque(true);
 			checkerBoard.add(spots[i][j]);
 		}
